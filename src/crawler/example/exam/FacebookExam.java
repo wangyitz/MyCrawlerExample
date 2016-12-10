86b6301ba92fcff051c6c800c9b82cef43042832
wangyitz <onlylike8@gmail.com>
2016-12-10 16:23:54 +0800

    HAHA總數

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -24,9 +24,9 @@ public class FacebookExam {
 		// 遠端資料路徑
 
 		String uri = 
-				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				"https://graph.facebook.com/v2.8"
+				+ "/crazyck101/posts?fields=id,message,created_time,reactions.type(HAHA).limit(0).summary(total_count)&since=1480849200&until=1480856400"
+				+ "&access_token=EAACEdEose0cBAHH4v0o4Tb0vg9aZCpSB0JYdfiysxDtcEldzVgSm2CdvsNrdaBJkYMHS10a6ec6taBZAqoBevdXIdO4vij7RwyoDPUvej5Ln8Ryxb9ZBQhNamWPrDksZCud6jYbTnbD20CCBu6ed2BIVZAebb03fbnu7uT840ZAgZDZD";
 
 
 		Elements elems =
@@ -34,19 +34,20 @@ public class FacebookExam {
 				.getFromJson(uri)
 				.select("data");
 		
-		String output = "id,reactions";
+
+		String output = "id,內容,發布日期按，按HAHA總數\n";
 
 		// 遂筆處理
 		for( Element data: elems ){
 			String id = data.select("id").text();
+			String message = data.select("message").text();
+			String created_time = data.select("created_time").text();
+			String reactions = data.select("total_count").text();
 
-			// FIXIT
-			String reactions = "";
-
-
-			output += id + "," + reactions + "\n";
+			output += id+",\""+message+"\","+created_time+","+reactions+"\n";
 		}
 
 		System.out.println( output );
+
 	} 
 }
