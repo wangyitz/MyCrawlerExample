5cc555caf303949a6d0611257f96ed762fb33feb
wangyitz <onlylike8@gmail.com>
2016-12-11 11:59:03 +0800

    ptt

--- src/crawler/example/pttexam.java
+++ src/crawler/example/pttexam.java
@@ -2,6 +2,8 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -24,22 +26,24 @@ public class pttexam {
 		// 遠端資料路徑
 		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481382833.A.7CE.html";
 
-		System.out.println(
-				CrawlerPack.start()
-				
-				// 參數設定
-			    .addCookie("over18","1")	// 設定cookie
-				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
-				
-				// 選擇資料格式 (三選一)
-				//.getFromJson(uri)
-			    .getFromHtml(uri)
-			    //.getFromXml(uri)
+		//System.out.println(
+				Document jsoup =CrawlerPack.start()
+							    .addCookie("over18","1")	// 設定cookie
+							    .getFromHtml(uri);
+
 			    
 			    // 這兒開始是 Jsoup Document 物件操作
 			    //.select(" .push ")
 				//.select(" span[class*='push']")
-				.select("span:containsOwn(推)")
-		);
+				//.select("span:containsOwn(推)+span")
+
+
+
+				for(Element elem : jsoup.select("#main-content *")) {
+						elem.remove();
+				}
+				System.out.println(jsoup.select("#main-content").text());
+		}
+		//);
 	}
-}
+
